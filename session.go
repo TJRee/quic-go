46a111f70d0aa36d3bdce2e3ef098f9df40562d2
Marten Seemann <martenseemann@gmail.com>
2020-02-10 14:15:50 +0800

    refactor packing of packets before and after the handshake is confirmed

--- session.go
+++ session.go
@@ -164,6 +164,7 @@ type session struct {
 	earlySessionReadyChan chan struct{}
 	handshakeCompleteChan chan struct{} // is closed when the handshake completes
 	handshakeComplete     bool
+	handshakeConfirmed    bool
 
 	receivedRetry       bool
 	receivedFirstPacket bool
@@ -1137,6 +1138,9 @@ func (s *session) handleCloseError(closeErr closeError) {
 }
 
 func (s *session) dropEncryptionLevel(encLevel protocol.EncryptionLevel) {
+	if encLevel == protocol.EncryptionHandshake {
+		s.handshakeConfirmed = true
+	}
 	s.sentPacketHandler.DropPackets(encLevel)
 	s.receivedPacketHandler.DropPackets(encLevel)
 }
@@ -1245,7 +1249,7 @@ sendLoop:
 }
 
 func (s *session) maybeSendAckOnlyPacket() error {
-	packet, err := s.packer.MaybePackAckPacket()
+	packet, err := s.packer.MaybePackAckPacket(s.handshakeConfirmed)
 	if err != nil {
 		return err
 	}
@@ -1303,7 +1307,13 @@ func (s *session) sendPacket() (bool, error) {
 	}
 	s.windowUpdateQueue.QueueAll()
 
-	packet, err := s.packer.PackPacket()
+	var packet *packedPacket
+	var err error
+	if !s.handshakeConfirmed {
+		packet, err = s.packer.PackPacket()
+	} else {
+		packet, err = s.packer.PackAppDataPacket()
+	}
 	if err != nil || packet == nil {
 		return false, err
 	}
